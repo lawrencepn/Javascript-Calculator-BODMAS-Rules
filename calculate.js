bbc456d059784422ec84e123c7e52bece0c1294d
Lawrence N. <lawrencenyakiso@gmail.com>
2015-02-18 15:18:15 +0200

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,38 @@
+//Handle Multiply
+                for (i = 0; i <= self.cArr.length; i++) {
+                    cItem = self.cArr[ i ];
+                    if (cItem == '*') {
+                        tLeft = parseFloat(self.cArr[ i - 1 ]);
+                        tRight = parseFloat(self.cArr[ i + 1 ]);
+
+                        nVal = tLeft * tRight;
+                        self.cArr[ i - 1 ] = nVal;
+                        self.cArr.splice(i, 2);
+                        i = self.cArr.length;
+                    }
+                }
+
+                //Handle Divide
+                for (i = 0; i <= self.cArr.length; i++) {
+                    cItem = self.cArr[ i ];
+                    if (cItem == '/') {
+                        tLeft = parseFloat(self.cArr[ i - 1 ]);
+                        tRight = parseFloat(self.cArr[ i + 1 ]);
+
+                        nVal = tLeft / tRight;
+                        self.cArr[ i - 1 ] = nVal;
+                        self.cArr.splice(i, 2);
+                        i = self.cArr.length;
+                    }
+                }
+
+                //Handle Plus and Minus
+                var tResult = parseFloat(self.cArr[ 0 ]);
+                for (i = 1; i < self.cArr.length; i++) {
+                    if (self.cArr[ i ] == '+') {
+                        tResult = tResult + parseFloat(self.cArr[ i + 1 ]);
+                    } else if (self.cArr[ i ] == '-') {
+                        tResult = tResult - parseFloat(self.cArr[ i + 1 ]);
+                    }
+                    i++;
+                }
